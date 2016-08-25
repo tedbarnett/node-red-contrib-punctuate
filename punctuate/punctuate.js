2d65af62f3c482c0e978c54a197637763e599bac
Ted Barnett <tedbarnett@users.noreply.github.com>
2016-08-25 00:42:22 -0700

    Included pause-replacement (with period) and capitalization of first word following the pause

--- punctuate/punctuate.js
+++ punctuate/punctuate.js
@@ -2,13 +2,28 @@ module.exports = function(RED) {
     function punctuateNode(config) {
     RED.nodes.createNode(this, config);
     var node = this;
+    var pauseString = msg.pause;
+    if (pauseString = '') pauseString = / %HESITATION/gi; // if pause not specified, use regex for the pause string from Watson transcriptions
     this.on('input', function (msg) {
-        msg.punctuated = msg.transcription.replace(/%HESITATION/g, '.');
-        node.send(msg);
-        // TO ADD:
-        // - Enable msg.pause input
-        // - Insert Watson-derived code here to do punctuation (see https://github.com/watson-developer-cloud/speech-javascript-sdk/blob/master/speech-to-text/format-stream.js)
+        msg.punctuated = msg.transcription.trim();
+        msg.punctuated = msg.punctuated.replace(/%HESITATION/g, '.');
 
+        msg.punctuated = msg.punctuated.trim();
+        msg.punctuated = msg.punctuated.replace(pauseString, '.'); // replace "pause" string with a period
+        
+        // Capitalize the first word that appears after every end-of-sentence mark (period, ?, !)
+        arr = msg.punctuated.split('');
+        capitalize = true;
+        for (x = 0; x < arr.length; x++)
+            if (arr[x] == '.' || arr[x] == '!' || arr[x] == '?')
+                capitalize = true;
+            else if (arr[x] != ' ' && capitalize)
+                {
+                    arr[x] = arr[x].toUpperCase();
+                    capitalize = false;
+                }
+        msg.punctuated = arr.join('');
+        node.send(msg);
     });
   }
   RED.nodes.registerType('punctuate', punctuateNode);
